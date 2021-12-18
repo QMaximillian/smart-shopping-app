1caf64ef7eee1abeee8e481328bcb70f568da04b
“Quinn <quinnlashinsky@gmail.com>
2021-12-17 23:06:30 -0500

    precommit hooks working

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,10 @@
-import type { MetaFunction, LoaderFunction } from "remix";
-import { useLoaderData, json, Link } from "remix";
+import type {MetaFunction, LoaderFunction} from 'remix'
+import {useLoaderData, json, Link} from 'remix'
 
 type IndexData = {
-  resources: Array<{ name: string; url: string }>;
-  demos: Array<{ name: string; to: string }>;
-};
+  resources: Array<{name: string; url: string}>
+  demos: Array<{name: string; to: string}>
+}
 
 // Loaders provide data to components and are only ever called on the server, so
 // you can connect to a database or run any server side code you want right next
@@ -14,56 +14,55 @@ export let loader: LoaderFunction = () => {
   let data: IndexData = {
     resources: [
       {
-        name: "Remix Docs",
-        url: "https://remix.run/docs"
+        name: 'Remix Docs',
+        url: 'https://remix.run/docs',
       },
       {
-        name: "React Router Docs",
-        url: "https://reactrouter.com/docs"
+        name: 'React Router Docs',
+        url: 'https://reactrouter.com/docs',
       },
       {
-        name: "Remix Discord",
-        url: "https://discord.gg/VBePs6d"
-      }
+        name: 'Remix Discord',
+        url: 'https://discord.gg/VBePs6d',
+      },
     ],
     demos: [
       {
-        to: "demos/actions",
-        name: "Actions"
+        to: 'demos/actions',
+        name: 'Actions',
       },
       {
-        to: "demos/about",
-        name: "Nested Routes, CSS loading/unloading"
+        to: 'demos/about',
+        name: 'Nested Routes, CSS loading/unloading',
       },
       {
-        to: "demos/params",
-        name: "URL Params and Error Boundaries"
-      }
-    ]
-  };
+        to: 'demos/params',
+        name: 'URL Params and Error Boundaries',
+      },
+    ],
+  }
 
   // https://remix.run/api/remix#json
-  return json(data);
-};
+  return json(data)
+}
 
 // https://remix.run/api/conventions#meta
 export let meta: MetaFunction = () => {
   return {
-    title: "Remix Starter",
-    description: "Welcome to remix!"
-  };
-};
+    title: 'Remix Starter',
+    description: 'Welcome to remix!',
+  }
+}
 
 // https://remix.run/guides/routing#index-routes
 export default function Index() {
-  let data = useLoaderData<IndexData>();
+  let data = useLoaderData<IndexData>()
 
   return (
     <div>
-      <main>
-      </main>
-      <aside>
-      </aside>
+      <main></main>
+      <aside></aside>
+      <div className="test"></div>
     </div>
-  );
+  )
 }
